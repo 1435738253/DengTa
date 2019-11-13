6c5674ec72cec594115178ce4a3ef82a40456b40
lingluo <1435738253@qq.com>
2019-11-13 09:53:49 +0800

    Signed-off-by: lingluo <1435738253@qq.com>

---
+++ MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication3;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //默认推荐内容
+    private String[] mStrs = {"Java", "Python", "c", "c++", "java", "python"};
+    private SearchView mSearchView;
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSearchView = (SearchView) findViewById(R.id.searchView);
+        mListView = (ListView) findViewById(R.id.listView);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrs));
+        mListView.setTextFilterEnabled(true);
+
+        // 设置搜索文本监听
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // 当点击搜索按钮时触发该方法
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            // 当搜索内容改变时触发该方法
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)){
+                    mListView.setFilterText(newText);
+                }else{
+                    mListView.clearTextFilter();
+                }
+                return false;
+            }
+        });
+
+    }
+}
+
